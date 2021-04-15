62fd60af067a6632eb78df8fdc45e4c9705d23ef
vonGorski <62354549+vonGorski@users.noreply.github.com>
2021-04-15 18:19:32 +0200

    Add project files.

---
+++ BD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace KG_245079_Kartkowka
+{
+    public class Movie
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+    }
+
+    public class Theater : DbContext
+    {
+        public virtual DbSet<Movie> Movies { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace KG_245079_Kartkowka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Theater();
+            // obszar zakomentowany aby nie dublować rejestrów
+         //   context.Movies.Add(new Movie { name = "Avatar"});
+         //   context.Movies.Add(new Movie { name = "Zorro"});
+         //   context.Movies.Add(new Movie { name = "Miodowe Lata"});
+         //   context.SaveChanges();
+            
+            // wyswietlanie tabeli
+            var movies = (from s in context.Movies select s).ToList<Movie>();
+            foreach (var st in movies)
+            {
+                Console.WriteLine("ID: {0}, Movie Name: {1}", st.id, st.name);
+            }
+            Console.Write("Nacisnij Enter by zakonczyc program");
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            { }
+        }
+    }
+}
